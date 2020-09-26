93aaffda94068508eaeee5a5bbb72b0658fb7abe
NightHunter311 <obliviongod16@gmail.com>
2020-09-27 00:51:29 +0530

    Working project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  componentDidMount(){
+    this.timeHandler()
+  }
+  componentDidUpdate(){
+    setInterval(() => {this.timeHandler()},1000)
+  }
+  state = {
+    time:"",    
+  }
+  timeHandler = () => {
+    var d = new Date();
+    var h = d.getHours()
+    if(h < 10){
+      h = `0${h}`
+    }
+    var m = d.getMinutes()
+    if(m < 10){
+      m = `0${m}`
+    }
+    var s = d.getSeconds()
+    if(s < 10){
+      s = `0${s}`
+    }
+    var t = `${h}${m}${s}`
+    console.log(t)
+    this.setState({time: t})
+  }
+  render() {
+    return (
+      <div style = {{backgroundColor: `#${this.state.time}`, color: "white", width: '100vw', height: '100vh', textAlign: 'center', verticalAlign: 'middle', lineHeight: '100vh', fontSize: '5vh'}}>
+        {`#${this.state.time}`}       
+      </div>
+    )
+  }
 }
 
 export default App;
