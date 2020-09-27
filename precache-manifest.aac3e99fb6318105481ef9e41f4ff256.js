d0b44e2e746fbf988e762bc020eb7f05998f4aea
NightHunter311 <obliviongod16@gmail.com>
2020-09-28 00:13:02 +0530

    Updates

---
+++ precache-manifest.aac3e99fb6318105481ef9e41f4ff256.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c33ce8c037743e0a0c082034980b94d8",
+    "url": "/ColorClock/index.html"
+  },
+  {
+    "revision": "b64800bec42a7c6863a1",
+    "url": "/ColorClock/static/css/main.7738ace9.chunk.css"
+  },
+  {
+    "revision": "32820364c0ca686047d1",
+    "url": "/ColorClock/static/js/2.1417f610.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ColorClock/static/js/2.1417f610.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b64800bec42a7c6863a1",
+    "url": "/ColorClock/static/js/main.d42e2982.chunk.js"
+  },
+  {
+    "revision": "e51300416487c5e992f8",
+    "url": "/ColorClock/static/js/runtime-main.64774e14.js"
+  }
+]);
